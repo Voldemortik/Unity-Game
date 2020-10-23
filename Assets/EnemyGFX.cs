84fc90a49d449fdc876113c5c49e0c55b8dc72d8
Anton <lesnoydyadya@gmail.com>
2020-10-23 04:09:29 -0700

    second commit

--- Assets/EnemyGFX.cs
+++ Assets/EnemyGFX.cs
@@ -5,17 +5,24 @@ using Pathfinding;
 
 public class EnemyGFX : MonoBehaviour
 {
+    Animator anim;
     public AIPath aiPath;
 
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
         if(aiPath.desiredVelocity.x >= 0.01f)
         {
+            anim.SetInteger("anim", 1);
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
         else if(aiPath.desiredVelocity.x <= -0.01f)
         {
+            anim.SetInteger("anim", 1);
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
     }
---
+++ Assets/HeroHP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class HeroHP : MonoBehaviour
+{
+    public Transform Spawn;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            collision.transform.position = Spawn.position;
+        }
+    }
+
+}
+
+
