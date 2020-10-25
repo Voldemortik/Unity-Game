5fe88ad6a7d8785c8cf0c68f841bbf308b970d90
Voldemortik <lesnoydyadya@gmail.com>
2020-10-25 18:03:59 +0200

    3 commit

---
+++ Assets/Camere_Auto_Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camere_Auto_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = 0.001F;
+
+        var cameraPosition = Camera.main.gameObject.transform.position;
+        cameraPosition.x += step;
+        Camera.main.gameObject.transform.position = cameraPosition;
+    }
+}
